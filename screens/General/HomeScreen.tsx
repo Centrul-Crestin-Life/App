0c2912ef8df584ccf67a56af91a71feb68d8b4cf
Davethewave140 <daviduz1999@gmail.com>
2022-10-06 23:52:56 +0200

    Update HomeScreen.tsx
    Renamed file for better usage

--- screens/General/HomeScreen.tsx
+++ screens/General/HomeScreen.tsx
@@ -1,7 +1,8 @@
 import { StyleSheet } from 'react-native'
 import { Text, View } from '../../components/Themed';
+import { RootTabScreenProps } from '../../types';
 
-export default function HomeScreen() {
+export default function HomeScreen({ navigation }: RootTabScreenProps<'Home'>) {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Hi, this is your Home screen.</Text>
