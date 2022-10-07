0475fbe673553095b92661158523da861927ceb6
Davethewave140 <daviduz1999@gmail.com>
2022-10-07 14:54:58 +0200

    removed old deprecated example code

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -7,7 +7,6 @@ import 'react-native-gesture-handler';
 import { FontAwesome5 } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import * as React from 'react';
 import { ColorSchemeName, Pressable, View, Text, Button } from 'react-native';
 import Colors from '../constants/Colors';
@@ -15,8 +14,6 @@ import useColorScheme from '../hooks/useColorScheme';
 import HomeScreen from '../screens/General/HomeScreen';
 import CalendarScreen from '../screens/General/CalendarScreen';
 import AnnouncementScreen from '../screens/Tineret/AnnouncementScreen';
-import ModalScreen from '../screens/ModalScreen';
-import NotFoundScreen from '../screens/NotFoundScreen';
 import { GeneralStackParamList, GeneralTabParamList, GeneralTabScreenProps, TineretStackParamList, TineretTabParamList, TineretTabScreenProps } from '../types';
 import LinkingConfiguration from './LinkingConfiguration';
 import {
@@ -34,24 +31,6 @@ export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeNa
       theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <MyDrawer />
     </NavigationContainer>
-  );
-}
-
-/**
- * A root stack navigator is often used for displaying modals on top of all other content.
- * https://reactnavigation.org/docs/modal
- */
-const Stack = createNativeStackNavigator<GeneralStackParamList>();
-
-function GeneralNavigator() {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="Root" component={BottomTabNavigatorGeneral} options={{ headerShown: false }} />
-      <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
-      <Stack.Group screenOptions={{ presentation: 'modal' }}>
-        <Stack.Screen name="Modal" component={ModalScreen} />
-      </Stack.Group>
-    </Stack.Navigator>
   );
 }
 
