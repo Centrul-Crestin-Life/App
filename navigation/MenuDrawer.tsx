236ca695d000f55d55032f7f5eda431272af1976
Davethewave140 <daviduz1999@gmail.com>
2022-12-23 17:46:28 +0100

    Update MenuDrawer.tsx
    made aligning and simplified things a bit

--- navigation/MenuDrawer.tsx
+++ navigation/MenuDrawer.tsx
@@ -1,6 +1,6 @@
 import { FontAwesome5 } from '@expo/vector-icons';
 import * as React from 'react';
-import {  Pressable } from 'react-native';
+import {  Pressable, Image, View, Text, StyleSheet } from 'react-native';
 import Colors from '../constants/Colors';
 import useColorScheme from '../hooks/useColorScheme';
 import LinkingConfiguration from './LinkingConfiguration';
@@ -11,6 +11,7 @@ import {
 } from '@react-navigation/drawer';
 import BottomTabNavigatorGeneral from './BottomTabNavigators/BottomTabNavigatorGeneral'
 import BottomTabNavigatorTineret from './BottomTabNavigators/BottomTabNavigatorTineret'
+import StackNavigatorLogin from './StackNavigators/StackNavigatorLogin';
 
 function CustomDrawerContent(props: any) {
   return (
@@ -40,14 +41,38 @@ export default function MenuDrawer() {
             style={{ marginLeft: 15 }}
           />
         </Pressable>,
-        headerTitleAlign: 'center'
+        headerRight: () => <Image
+              source={require('../assets/images/Icon-yellow.png' )}
+              style={{height: 50, width: 50}}
+              resizeMode='cover'
+            />,
+            /*height: 55, width: 55, marginLeft: 0, paddingRight: 500 */
+        headerTitle: () => (
+          <FontAwesome5
+            color={Colors[colorScheme].text}
+            style={styles.title}>
+            Centrul Crestin Life
+          </FontAwesome5>
+        ),
+        headerTitleAlign: 'left',
       })}
       useLegacyImplementation
       drawerContent={(props) => <CustomDrawerContent {...props} /> } >
       
-        
-      <Drawer.Screen name="General" component={BottomTabNavigatorGeneral} />
+      <Drawer.Screen name={StackNavigatorLogin.name} component={StackNavigatorLogin} />  
+      <Drawer.Screen name="Centrul Crestin Life" component={BottomTabNavigatorGeneral} />
       <Drawer.Screen name="Tineret" component={BottomTabNavigatorTineret} />
     </Drawer.Navigator>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'normal'
+  }
+});  
